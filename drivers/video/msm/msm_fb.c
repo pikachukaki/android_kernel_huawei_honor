38764a11e8b148ed2d5d4472d4e9eeb9800ce75f
pikachukaki <pikachukaki@gmail.com>
2013-02-24 00:22:11 +0200

    msm_fb: clean fb to prevent displaying old fb
    clean fb to prevent displaying old fb on resume.

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -991,6 +991,11 @@ static int msm_fb_blank_sub(int blank_mode, struct fb_info *info,
 			mfd->op_enable = FALSE;
 			curr_pwr_state = mfd->panel_power_on;
 			mfd->panel_power_on = FALSE;
+			
+			/* clean fb to prevent displaying old fb */
+			memset((void *)info->screen_base, 0,
+					info->fix.smem_len);
+					
 /* Remove qcom backlight mechanism,user our own */
 #ifndef CONFIG_HUAWEI_KERNEL
 			bl_updated = 0;

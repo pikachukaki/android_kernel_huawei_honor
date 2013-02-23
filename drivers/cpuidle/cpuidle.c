3a96bf6296ec41065e0acac020f572ceaa0b2877
pikachukaki <pikachukaki@gmail.com>
2013-02-23 23:47:11 +0200

    cpuidle: Add last_residency trace event for residency profiling

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -103,7 +103,7 @@ static void cpuidle_idle_call(void)
 	dev->last_residency = target_state->enter(dev, target_state);
 
 	trace_power_end(dev->cpu);
-	trace_cpu_idle(PWR_EVENT_EXIT, dev->cpu);
+	trace_cpu_residency(dev->last_residency);
 
 	if (dev->last_state)
 		target_state = dev->last_state;

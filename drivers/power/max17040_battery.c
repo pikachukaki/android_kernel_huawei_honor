b30cb9b8c9479a00745de786b6d362049a5b91b3
pikachukaki <pikachukaki@gmail.com>
2013-02-23 23:40:40 +0200

    Added Battery Life eXtender (BLX) version 1. - From Ezekeel

--- drivers/power/max17040_battery.c
+++ drivers/power/max17040_battery.c
@@ -21,6 +21,10 @@
 #include <linux/max17040_battery.h>
 #include <linux/slab.h>
 
+#ifdef CONFIG_BLX
+#include <linux/blx.h>
+#endif
+
 #define MAX17040_VCELL_MSB	0x02
 #define MAX17040_VCELL_LSB	0x03
 #define MAX17040_SOC_MSB	0x04
@@ -171,7 +175,10 @@ static void max17040_get_status(struct i2c_client *client)
 	} else {
 		chip->status = POWER_SUPPLY_STATUS_DISCHARGING;
 	}
-
+	
++#ifdef CONFIG_BLX
+	if (chip->soc >= get_charginglimit())
+#else
 	if (chip->soc > MAX17040_BATTERY_FULL)
 		chip->status = POWER_SUPPLY_STATUS_FULL;
 }
